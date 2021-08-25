dbd795b93bd9f3d957ea26a94539492053c427e6
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2021-08-25 11:23:47 +0200

    Step 4 - Ensure unique value

--- server.js
+++ server.js
@@ -5,29 +5,33 @@ const app = express();
 
 //Database
 mongoose
-  .connect("mongodb://127.0.0.1:27017/wilderdb")
+  .connect("mongodb://127.0.0.1:27017/wilderdb", {
+    autoIndex: true,
+  })
   .then(() => console.log("Connected to database"))
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("Hello World");
   // test a wilder creation
-  const firstWilder = new WilderModel({
-    name: "First Wilder",
-    city: "San Francisco",
-    skills: [
-      { title: "HTML", votes: 10 },
-      { title: "React", votes: 5 },
-    ],
-  });
-  firstWilder
-    .save()
-    .then((result) => {
-      console.log("success:", result);
-    })
-    .catch((err) => {
-      console.log("error:", err);
+  WilderModel.init().then(() => {
+    const firstWilder = new WilderModel({
+      name: "First Wilder",
+      city: "San Francisco",
+      skills: [
+        { title: "HTML", votes: 10 },
+        { title: "React", votes: 5 },
+      ],
     });
+    firstWilder
+      .save()
+      .then((result) => {
+        console.log("success:", result);
+      })
+      .catch((err) => {
+        console.log("error:", err);
+      });
+  });
 });
 
 //Start Server
